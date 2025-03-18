69c0d9d5e6d8a63fe7e27e73cad0b126a884368b
Sunjoo Park <all4dich@gmail.com>
2025-03-18 10:03:05 +0900

    Add a function to resize an input image

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ import sys
 # Get model path from an argument
 model_path = sys.argv[1]
 
+def letter_box(image_src, new_shape=(512, 512), fill_color=(114, 114, 114), format=None):
+    
+    src_shape = image_src.shape[:2] # height, width
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    r = min(new_shape[0] / src_shape[0], new_shape[1] / src_shape[1])
+
+    ratio = r, r  
+    new_unpad = int(round(src_shape[1] * r)), int(round(src_shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  
+
+    dw /= 2 
+    dh /= 2
+
+    if src_shape[::-1] != new_unpad:  
+        image_src = cv2.resize(image_src, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    image_new = cv2.copyMakeBorder(image_src, top, bottom, left, right, cv2.BORDER_CONSTANT, value=fill_color)  # add border
+    if format is not None:
+        image_new = cv2.cvtColor(image_new, format)
+    
+    return image_new, ratio, (dw, dh)    
+
 if __name__ == "__main__":
     engine = InferenceEngine(model_path)
     print(engine)
