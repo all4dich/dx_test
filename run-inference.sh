079858b3035ac06b91828a0aafd678bc6d512a86
Sunjoo Park <all4dich@gmail.com>
2025-04-02 14:29:08 +0900

    Enhance run-inference script with dynamic file path handling and error checks

---
+++ run-inference.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get the current script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Get the absolute path of the dxnn file. If not defined use the default path
+if [ -z "$1" ]; then
+  echo "INFO: dxnn file path not provided, using default path."
+  DXNN_FILE_PATH="${HOME}/workspace/simulator/examples/compiled_results/yolov5s.dxnn"
+else
+  DXNN_FILE_PATH=$(realpath "$1")
+fi
+# Get the absolute path of the mp4 file
+# IF NOT DEFINED USE THE DEFAULT PATH
+if [ -z "$2" ]; then
+  echo "INFO: mp4 file path not provided, using default path."
+  MP4_FILE_PATH="${HOME}/Airshow.mp4"
+else
+  # Check if the file exists
+  if [ ! -f "$2" ]; then
+    echo "Error: File $2 does not exist."
+    exit 1
+  fi
+fi
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "Error: Python virtual environment is not activated."
+  echo "INFO: Activating virtual environment..."
+  if [ -d ".venv" ]; then
+      source .venv/bin/activate
+  else
+      source ${HOME}/program/venv/bin/activate
+  fi
+fi
+python3 ./main.py ${DXNN_FILE_PATH} ${MP4_FILE_PATH}
